1e3dcb46779074dc5d41e84e63ab42a223135d7e
Nico <nicolasmuniz2005@gmail.com>
2025-03-10 16:13:47 +0100

    Commit inicial. Ficheros con la estructura de los algoritmos de encriptación

---
+++ monoalfabeto.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def inputManager() -> None:
+    return
+
+
+def main() -> None:    
+    return
+
+
+if __name__ == "__main__":
+    main()
---
+++ vigenere.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def inputManager() -> None:
+    return
+
+
+def main() -> None:    
+    return
+
+
+if __name__ == "__main__":
+    main()
